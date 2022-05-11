d7485022535298bc25a43efdd4d8583581322d2a
André de Faria Machado Eletherio <andremachadoeletherio@gmail.com>
2022-05-11 19:44:56 -0300

    Strings e Arrays

---
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,48 @@
+// Exercícios de interpretação
+
+//1.
+// a. undefinid
+// b. null
+// c. 11
+// d. 3
+// e. 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13
+// f. 9
+
+
+//2.
+// SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+
+
+// Exercícios de Escrita
+
+// Exercício 1
+// const nome = prompt("Qual é o seu nome? ");
+// const email = prompt("Qual é o seu e-mail? ");
+
+// console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o) ${nome}`);
+
+
+// Exercício 2
+// const comidas = ["Pizza", "Hamburger", "Lasanha", "Strogonoff", "Churrasco"];
+// console.log(comidas);
+// console.log(`essas são as minhas comidas favoritas:\n${comidas[0]}\n${comidas[1]}\n${comidas[2]}\n${comidas[3]}\n${comidas[4]}`);
+// const comidaUsuario = prompt("Qual a sua comida preferida? ");
+// comidas[1] = comidaUsuario;
+// console.log(comidas);
+
+
+// Exercício 3
+// const listaDeTarefas = [];
+// const tarefa1 = prompt("Primeira Tarefa que precisa realizar no dia:");
+// const tarefa2 = prompt("Segunda Tarefa que precisa realizar no dia:");
+// const tarefa3 = prompt("Terceira Tarefa que precisa realizar no dia:");
+
+// listaDeTarefas[1] = tarefa1;
+// listaDeTarefas[2] = tarefa2;
+// listaDeTarefas[3] = tarefa3;
+// console.log(listaDeTarefas);
+
+// const tarefaRealizada = prompt("Digite o índice de uma atividade que já realizou: ");
+// listaDeTarefas.splice(tarefaRealizada, 1);
+// console.log(listaDeTarefas);
