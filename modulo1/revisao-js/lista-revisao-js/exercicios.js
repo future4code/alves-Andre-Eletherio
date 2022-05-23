175291826a4d4b861b57bd2945e4eadff0c4b437
André de Faria Machado Eletherio <andremachadoeletherio@gmail.com>
2022-05-23 11:08:00 -0300

    Exercicio-03

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -15,8 +15,8 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  
-}
+    return array.sort((a, b) => a - b)
+};
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
