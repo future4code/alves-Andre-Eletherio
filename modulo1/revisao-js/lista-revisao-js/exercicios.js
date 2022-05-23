63d32a65ab6d72f05d52a08d1ccf3110c06e19c6
André de Faria Machado Eletherio <andremachadoeletherio@gmail.com>
2022-05-23 11:54:35 -0300

    Exercicio-09

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -79,12 +79,18 @@ function retornaNPrimeirosPares(n) {
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+    if (ladoA === ladoB && ladoB === ladoC) {
+        return "Equilátero";
+    } else if (ladoA !== ladoB && ladoA !== ladoC && ladoB !== ladoC) {
+        return "Escaleno";
+    } else {
+        return "Isósceles";
+    }
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+    
 }
 
 // EXERCÍCIO 11
