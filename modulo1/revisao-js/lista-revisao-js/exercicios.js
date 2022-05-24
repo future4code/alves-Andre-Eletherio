0d474c73c633409abb9c6f0c2e50600ecea6d886
André de Faria Machado Eletherio <andremachadoeletherio@gmail.com>
2022-05-23 23:24:56 -0300

    Desafio-Exercicio-04

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -33,7 +33,13 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-    return array.filter((number) => number % 2 === 0);
+    let arrayPares = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            arrayPares.push(array[i]);
+        }
+    }
+    return arrayPares;
 }
 
 // EXERCÍCIO 05
