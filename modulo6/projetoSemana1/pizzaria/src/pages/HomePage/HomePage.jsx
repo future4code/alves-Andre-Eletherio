77fe6ec0e7f1a354d43fe3ad4f2a854b72902cdd
André Eletherio <andremachadoeletherio@gmail.com>
2022-10-10 23:41:48 -0300

    Inicio

---
+++ modulo6/projetoSemana1/pizzaria/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/header/Header";
+
+export function HomePage() {
+
+    return (
+        <section>
+            <Header/>
+            
+        </section>
+    )
+}
